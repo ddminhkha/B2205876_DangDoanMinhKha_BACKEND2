4ebd4bab63cd02387dfe63f51f882e128838083b
ddminhkha <khab2205876@student.ctu.edu.vn>
2025-10-19 21:34:56 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const contactsRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/api-error');
 
 const app = express();
 
@@ -8,8 +9,19 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to contact book application' });
+    res.json({ message: 'Welcome to the Contact book application' });
 });
 app.use('/api/contacts', contactsRouter);
+
+// Handle 404 responses
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
+    });
+});
 
 module.exports = app;
